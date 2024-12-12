d7d2adc06b81ffcd58a8c146cd7da118badd22d7
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-12-12 18:18:53 -0500

    se crea admin para visualizacion #3

--- imdb_clone/genres/admin.py
+++ imdb_clone/genres/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from genres.models import Genre
+
+
+# admin para Género
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+admin.site.register(Genre, GenreAdmin)
