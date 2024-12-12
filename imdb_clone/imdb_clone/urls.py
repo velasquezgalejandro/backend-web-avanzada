7816db859d4a0472c9a5ec4803dc7d42c6fabca0
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-12-12 18:32:18 -0500

    logica para aplicacion de actores #1

--- imdb_clone/imdb_clone/urls.py
+++ imdb_clone/imdb_clone/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("genres.urls")),
+    path("api/", include("actors.urls")),
 ]
