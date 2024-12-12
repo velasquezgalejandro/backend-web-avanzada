254ac284aa780fb9f261b5939a8ad378ee7baa78
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-12-12 18:19:17 -0500

    se crean views #3

--- imdb_clone/genres/views.py
+++ imdb_clone/genres/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
 # Create your views here.
+# Vista para Género
+from rest_framework import viewsets
+from .models import Genre
+from .serializers import GenreSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
