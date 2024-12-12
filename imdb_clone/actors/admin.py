6be1d819a729ae7a32e120b0869d98f0ebf7e9cd
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-12-12 18:32:48 -0500

    creacion de admin de actores #1

--- imdb_clone/actors/admin.py
+++ imdb_clone/actors/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from actors.models import Actor
+
+
+# Admin para Actor
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ("name", "birth_date")
+    search_fields = ("name",)
+
+
+admin.site.register(Actor, ActorAdmin)
