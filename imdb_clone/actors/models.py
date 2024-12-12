1f4d33eefffb1fbc88b330e8661433c93deddf2b
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-12-12 18:32:29 -0500

    creacion de modelo de actores #1

--- imdb_clone/actors/models.py
+++ imdb_clone/actors/models.py
@@ -1,3 +1,11 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+
+# modelo de Actor
+class Actor(models.Model):
+    name = models.CharField(max_length=255)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return self.name
