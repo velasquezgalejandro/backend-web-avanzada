0e7df5466ee90c57118de4be46549dc5a7314786
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-12-12 18:33:29 -0500

    creacion de vista para actores #1

--- imdb_clone/actors/views.py
+++ imdb_clone/actors/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from actors.models import Actor
+from actors.serializers import ActorSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+
+# Vista para Actor
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
