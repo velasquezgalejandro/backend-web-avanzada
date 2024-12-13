f4bfc5152d111889d347d0b923a70e623f1bfaf1
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-12-13 14:50:06 -0500

    creacion de admin para rating #5

--- imdb_clone/rating/admin.py
+++ imdb_clone/rating/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from rating.models import Rating
+
+
+# admin para Rating
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ("movie", "user", "score")
+    search_fields = ("movie", "user")
+
+
+admin.site.register(Rating, RatingAdmin)
