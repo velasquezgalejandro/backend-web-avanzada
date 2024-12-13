4d9fce756e25b74552b7113cd456eba4bb6bee7f
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-12-13 14:51:03 -0500

    creacion de views para rating #5

--- imdb_clone/rating/views.py
+++ imdb_clone/rating/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import viewsets
+from rating.models import Rating
+from rating.serializers import RatingSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+
+# vista para Rating
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
