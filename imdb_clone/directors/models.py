73a4dc3ee0540459f3b29f6fe181e8d7f5912597
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-12-13 14:47:52 -0500

    creacion de modelos para directores #2

--- imdb_clone/directors/models.py
+++ imdb_clone/directors/models.py
@@ -1,3 +1,11 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+
+# modelo para Director
+class Director(models.Model):
+    name = models.CharField(max_length=255)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return self.name
