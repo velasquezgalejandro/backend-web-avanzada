aac6fa7ff20f8d44da3be510e166d737bf810653
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-12-13 14:49:06 -0500

    creacion de modelos para peliculas #4

--- imdb_clone/movies/models.py
+++ imdb_clone/movies/models.py
@@ -1,3 +1,18 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from actors.models import Actor
+from directors.models import Director
+from genres.models import Genre
+
+
+# modelo para Pelicula
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    release_date = models.DateField()
+    director = models.ForeignKey(Director, on_delete=models.SET_NULL, null=True)
+    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(Genre)
+
+    def __str__(self):
+        return self.title
