1b77349f790fd8627f8623dd48767b9afda39aa9
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-12-13 14:47:41 -0500

    creacion de admin para directores #2

--- imdb_clone/directors/admin.py
+++ imdb_clone/directors/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from directors.models import Director
+
+
+# admin para Director
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ("name", "birth_date")
+    search_fields = ("name",)
+
+
+admin.site.register(Director, DirectorAdmin)
