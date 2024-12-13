8b74fcb5c6ad23820f706ef03aecae3dbd0b86db
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-12-13 14:48:48 -0500

    creacion de admin para peliculas #4

--- imdb_clone/movies/admin.py
+++ imdb_clone/movies/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from movies.models import Movie
+
+
+# admin para Movie
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("title", "release_date", "director")
+    search_fields = ("title", "description")
+    list_filter = ("release_date", "director")
+
+
+admin.site.register(Movie, MovieAdmin)
