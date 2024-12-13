5ff8ea3e74c5be8169ea070980f918adb3d4a8ee
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-12-13 14:48:29 -0500

    creacion de views para directores #2

--- imdb_clone/directors/views.py
+++ imdb_clone/directors/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import viewsets
+from directors.models import Director
+from directors.serializers import DirectorSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+
+# Vista para Director
+class DirectorViewSet(viewsets.ModelViewSet):
+    queryset = Director.objects.all()
+    serializer_class = DirectorSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
