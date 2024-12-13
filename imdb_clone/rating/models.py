e258e9bd93e1c178c7f47621936cbe4e61a66e23
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-12-13 14:50:20 -0500

    creacion de modelo para rating #5

--- imdb_clone/rating/models.py
+++ imdb_clone/rating/models.py
@@ -1,3 +1,20 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from movies.models import Movie
+
+
+# modelo para rating
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie, related_name="ratings", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name="ratings", on_delete=models.CASCADE)
+    score = models.PositiveIntegerField()  # Calificación de 1 a 10, por ejemplo
+
+    class Meta:
+        unique_together = (
+            "movie",
+            "user",
+        )
+
+    def __str__(self):
+        return f"{self.user.username} - {self.movie.title}: {self.score}"
