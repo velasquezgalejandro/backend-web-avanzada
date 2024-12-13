d38bac84eaadd5d2d14fbf3b93d8b94cafe263c8
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-12-13 14:49:52 -0500

    creacion de views para peliculas #4

--- imdb_clone/movies/views.py
+++ imdb_clone/movies/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import viewsets
+from movies.models import Movie
+from movies.serializers import MovieSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+# from movies.permissions import IsOwnerOrReadOnly
+
+
+# vista para Pelicula
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
