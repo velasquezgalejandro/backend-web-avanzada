93430b882c719f7715e060dbc0c270da2bb025b4
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-12-19 17:58:31 -0500

    se agrega poster a movies #4

--- imdb_clone/movies/models.py
+++ imdb_clone/movies/models.py
@@ -13,6 +13,9 @@ class Movie(models.Model):
     director = models.ForeignKey(Director, on_delete=models.SET_NULL, null=True)
     actors = models.ManyToManyField(Actor)
     genres = models.ManyToManyField(Genre)
+    poster = models.ImageField(
+        upload_to="movie_posters/", blank=True, null=True
+    )  # Campo para la imagen
 
     def __str__(self):
         return self.title
